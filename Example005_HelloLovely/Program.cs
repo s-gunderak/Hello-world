17e2a711a42580dde15eaed6858f30a4f7c9fb48
gunderak <sergey.gund@gmail.com>
2023-01-24 22:24:26 +0300

    added Example 005 Hello Lovely

---
+++ Example005_HelloLovely/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "танечка")
+{
+    Console.Write("Ура, это же ");
+    Console.Write(username);
+    Console.WriteLine("!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
